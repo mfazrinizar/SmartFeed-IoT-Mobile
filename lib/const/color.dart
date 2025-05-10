15e4e91f035a68cb2703422c8700655abe06e520
M. Fazri Nizar <anon6372098.id@gmail.com>
2025-05-11 03:42:18 +0700

    Refactor: AppColors

--- lib/const/color.dart
+++ lib/const/color.dart
@@ -1,9 +1,12 @@
 import 'package:flutter/material.dart';
 
 class AppColors {
-  static const Color primary = Color(0xFF77DD77);
-  static const Color secondary = Color(0xFFADD8E6);
-  static const Color scaffoldBackground = Color(0xFF8FBC8F);
-  static const Color appBarBackground = Color(0xFF77DD77);
-  static const Color fabBackground = Color(0xFFFFD700);
+  static const Color primary = Color.fromARGB(255, 94, 202, 94);
+  static const Color secondary = Color.fromARGB(255, 173, 216, 230);
+  static const Color scaffoldBackground = Color.fromARGB(255, 143, 188, 143);
+  static const Color appBarBackground = Color.fromARGB(255, 119, 221, 119);
+  static const Color fabBackground = Color.fromARGB(255, 255, 215, 0);
+  static const Color commonBackground = Color.fromARGB(255, 235, 255, 235);
+  static const Color commonError = Color.fromARGB(255, 218, 71, 71);
+  static const Color commonSuccess = Color.fromARGB(255, 20, 152, 20);
 }

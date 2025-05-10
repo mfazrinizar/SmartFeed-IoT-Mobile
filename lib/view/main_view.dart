5f8dd2a8570de0e23f1dba6c7188d2728d516ba1
M. Fazri Nizar <anon6372098.id@gmail.com>
2025-05-11 03:49:28 +0700

    Refactor: main view to match other views

--- lib/view/main_view.dart
+++ lib/view/main_view.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:animated_notch_bottom_bar/animated_notch_bottom_bar/animated_notch_bottom_bar.dart';
 import 'package:smartfeed/const/color.dart';
+import 'package:smartfeed/view/notification_view.dart';
 import 'dashboard_view.dart';
 import 'history_view.dart';
 import 'settings_view.dart';
@@ -13,13 +14,22 @@ class MainView extends StatefulWidget {
 }
 
 class _MainViewState extends State<MainView> {
-  final NotchBottomBarController _controller = NotchBottomBarController();
-  final PageController _pageController = PageController();
+  late final NotchBottomBarController _controller;
+  late final PageController _pageController;
   final List<Widget> _pages = [
     DashboardView(),
     HistoryView(),
     SettingsView(),
   ];
+
+  int maxIndex = 2;
+
+  @override
+  void initState() {
+    super.initState();
+    _pageController = PageController(initialPage: 0);
+    _controller = NotchBottomBarController(index: 0);
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -29,34 +39,59 @@ class _MainViewState extends State<MainView> {
         physics: const NeverScrollableScrollPhysics(),
         children: _pages,
       ),
+      extendBody: false,
       bottomNavigationBar: AnimatedNotchBottomBar(
-        kIconSize: 30,
-        kBottomRadius: 30,
+        durationInMilliSeconds: 300,
+        kIconSize: 24.0,
+        kBottomRadius: 28.0,
         notchBottomBarController: _controller,
         color: AppColors.primary,
-        showLabel: true,
-        notchColor: AppColors.scaffoldBackground,
+        showLabel: false,
+        notchColor: AppColors.primary,
         removeMargins: false,
         bottomBarItems: const [
           BottomBarItem(
-            inActiveItem: Icon(Icons.dashboard_outlined, color: Colors.black),
-            activeItem: Icon(Icons.dashboard, color: Color(0xFFFFD700)),
+            inActiveItem: Icon(Icons.dashboard_outlined,
+                color: AppColors.commonBackground),
+            activeItem: Icon(Icons.dashboard, color: Colors.white),
             itemLabel: 'Dashboard',
           ),
           BottomBarItem(
-            inActiveItem: Icon(Icons.history, color: Colors.black),
-            activeItem: Icon(Icons.history, color: Color(0xFFFFD700)),
+            inActiveItem:
+                Icon(Icons.history, color: AppColors.commonBackground),
+            activeItem: Icon(Icons.history, color: Colors.white),
             itemLabel: 'History',
           ),
           BottomBarItem(
-            inActiveItem: Icon(Icons.settings_outlined, color: Colors.black),
-            activeItem: Icon(Icons.settings, color: Color(0xFFFFD700)),
+            inActiveItem: Icon(Icons.settings_outlined,
+                color: AppColors.commonBackground),
+            activeItem: Icon(Icons.settings, color: Colors.white),
             itemLabel: 'Settings',
           ),
         ],
         onTap: (index) {
-          _controller.jumpTo(index);
+          _pageController.jumpToPage(index);
         },
+      ),
+      floatingActionButton: ClipOval(
+        child: Material(
+          color: AppColors.primary,
+          child: InkWell(
+            splashColor: Colors.grey,
+            child: const SizedBox(
+              width: 50,
+              height: 50,
+              child: Icon(Icons.notifications, color: Colors.white, size: 30),
+            ),
+            onTap: () async {
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                    builder: (context) => const NotificationView()),
+              );
+            },
+          ),
+        ),
       ),
     );
   }
